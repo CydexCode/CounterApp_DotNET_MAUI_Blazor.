44e1f4f27d269b52984eb940b9ada1d8a9bfdb2a
Sachinthana Buddhika <112784979+CydexCode@users.noreply.github.com>
2024-07-07 12:53:07 +0530

    Add project files.

---
+++ CountValueApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CountValueApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ CountValueApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CountValueApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
